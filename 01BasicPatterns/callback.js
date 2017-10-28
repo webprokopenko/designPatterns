e1dc255ec8050ce9e1f0c7e976d76c05a0b4e24b
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-29 01:05:07 +0300

    BasicPatterns --callback async style

--- 01BasicPatterns/callback.js
+++ 01BasicPatterns/callback.js
@@ -5,3 +5,12 @@ function add(a,b, callback){
 console.log('before');
 add(1,2, result=>console.log('Result: ' + result));
 console.log('after');
+//Callback async style
+function additionAsync(a,b, callback){
+    setTimeout(()=>callback(a+b),100);
+}
+console.log('before async');
+additionAsync(1,2, function(result){
+    console.log('Result: ' + result)
+});
+console.log('after async');
